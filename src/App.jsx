576d70dcbc5070ed783c97842a7980942319ecd0
yogeshyogi634 <yogeshyogi634@gmail.com>
2024-08-23 18:51:12 +0530

    files added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Video from "./Pages/Video/Video";
+import Home from "./Pages/Home/Home";
+import { useState } from "react";
 
 function App() {
-  return <div>Hello world</div>;
+  const [sidebar, setSidebar] = useState(true);
+  return (
+    <div>
+      <Navbar setSidebar={setSidebar} />
+      <Routes>
+        <Route path="/" element={<Home sidebar={sidebar} />} />
+        <Route path="/video/:categoryId/:videoId" element={<Video />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,41 @@
+import "./Navbar.css";
+import menu from "../../assets/menu.png";
+// import logo from "../../assets/logo.png";
+import logo from "../../assets/youtube-logo.png";
+import search from "../../assets/search.png";
+import upload from "../../assets/upload.png";
+import more from "../../assets/more.png";
+import notification from "../../assets/notification.png";
+import profile from "../../assets/jack.png";
+
+const Navbar = ({ setSidebar }) => {
+  return (
+    <nav className="flex-div">
+      <div className="nav-left flex-div">
+        <img
+          src={menu}
+          alt=""
+          className="menu-icon"
+          onClick={() => setSidebar((prev) => !prev)}
+        />
+        <img src={logo} alt="" className="logo" />
+      </div>
+
+      <div className="nav-middle flex-div">
+        <div className="search-box flex-div">
+          <input type="text" placeholder="Search" />
+          <img src={search} alt="" />
+        </div>
+      </div>
+
+      <div className="nav-right flex-div">
+        <img src={upload} alt="" />
+        <img src={more} alt="" />
+        <img src={notification} alt="" />
+        <img src={profile} alt="" className="user-icon" />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;

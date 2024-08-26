65ae473b644cc0c303850ac412153734188e05f1
yogeshyogi634 <yogeshyogi634@gmail.com>
2024-08-26 22:54:46 +0530

    files added

--- src/Pages/Video/Video.jsx
+++ src/Pages/Video/Video.jsx
@@ -1,10 +1,12 @@
 import PlayVideo from "../../components/PlayVideo/PlayVideo";
+import Recommended from "../../components/Recommended/Recommended";
 import "./Video.css";
 
 const Video = () => {
   return (
     <div className="play-container">
       <PlayVideo />
+      <Recommended />
     </div>
   );
 };
